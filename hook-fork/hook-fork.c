1f51e2bb28d0cdaf7224229c7aaf3808205d2493
Amit Barzilai <amit.barzilai22@gmail.com>
2025-10-31 19:41:44 +0200

    Added hooked_fork with pt_regs option

--- hook-fork/hook-fork.c
+++ hook-fork/hook-fork.c
@@ -1,10 +1,45 @@
 #include <linux/init.h>
 #include <linux/module.h>
+#include <linux/sched.h>
+#include <linux/version.h>
 
 
 MODULE_LICENSE("Dual BSD/GPL");
 MODULE_AUTHOR("Amit Barzilai");
 MODULE_DESCRIPTION("Hooks the fork syscall and prints a to the kernel buffer the PID that called the syscall.");
+
+#if defined(CONFIG_X86_64) && (LINUX_VERSION_CODE >= KERNEL_VERSION(4,17,0))
+#define PTREGS_SYSCALL_STUBS 1
+#endif
+
+#ifdef PTREGS_SYSCALL_STUBS
+static asmlinkage pid_t (*orig_fork)(const struct pt_regs *);
+
+static asmlinkage pid_t hooked_fork(const struct pt_regs *registers)
+{
+    pid_t current_pid;
+    pid_t res;
+
+    current_pid = current->pid;
+    res = orig_fork(registers);
+
+    if (res <= -1)
+    {
+        printk(KERN_ALERT "hook-fork: error - sys_fork of %d returned %d", current_pid, res);
+        return res;
+    }
+    if (res == 0)
+    {
+        printk(KERN_INFO "hook-fork: child_process - sys_fork of %d returned %d", current_pid, res);
+        return res;
+    }
+
+    printk(KERN_INFO "hook-fork: parent_process - sys_fork of %d returned %d", current_pid, res);
+    return res;
+}
+#else
+
+#endif
 
 static int fork_hook_init(void)
 {
