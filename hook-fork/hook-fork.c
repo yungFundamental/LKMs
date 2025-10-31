c89ad4173330c5551bd304b458648a5e029bb25e
Amit Barzilai <amit.barzilai22@gmail.com>
2025-10-31 19:14:13 +0200

    Changed prints to hook info

--- hook-fork/hook-fork.c
+++ hook-fork/hook-fork.c
@@ -6,16 +6,16 @@ MODULE_LICENSE("Dual BSD/GPL");
 MODULE_AUTHOR("Amit Barzilai");
 MODULE_DESCRIPTION("Hooks the fork syscall and prints a to the kernel buffer the PID that called the syscall.");
 
-static int hello_init(void)
+static int fork_hook_init(void)
 {
-	printk(KERN_ALERT "Hello, world\n");
+	printk(KERN_INFO "Hooking the fork syscall\n");
 	return 0;
 }
 
-static void hello_exit(void)
+static void fork_hook_exit(void)
 {
-	printk(KERN_ALERT "Goodbye, cruel world\n");
+	printk(KERN_ALERT "Removing the fork syscall hook\n");
 }
 
-module_init(hello_init);
-module_exit(hello_exit);
+module_init(fork_hook_init);
+module_exit(fork_hook_exit);
