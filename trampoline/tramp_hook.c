c2c270c219d4c74c8d72ec3779c7be1376d34fe3
Amit Barzilai <amit.barzilai22@gmail.com>
2025-11-12 13:43:43 +0200

    Made page writable successfully!

--- trampoline/tramp_hook.c
+++ trampoline/tramp_hook.c
@@ -8,13 +8,30 @@
 #include <linux/kallsyms.h>
 #include <linux/types.h>
 #include <linux/mm.h>
+#include <asm/tlbflush.h>  
 
 
 MODULE_LICENSE("Dual BSD/GPL");
 MODULE_AUTHOR("Amit Barzilai");
 MODULE_DESCRIPTION("Trampoline hook");
 
+typedef pte_t *(*lookup_address_t)(unsigned long address, unsigned int *level);
+
 static u8 call_opcode = 0xe8;
+static lookup_address_t my_lookup_address;
+
+int make_page_writable(unsigned long address) {
+    unsigned int level;
+    pte_t *pte;
+    
+    pte = my_lookup_address(address, &level);
+    if (!pte) 
+	return -EINVAL;
+    
+    set_pte_atomic(pte, pte_mkwrite(*pte));
+    __flush_tlb_one(address);
+    return 0;
+}
 
 void hooker(void)
 {
@@ -28,6 +45,15 @@ static int sys_hooks_init(void)
     unsigned long hooked_func_page;
     struct page *page;
     char arr[4] = "abc";
+    my_lookup_address = (lookup_address_t)kallsyms_lookup_name("lookup_address");
+    
+    if (!my_lookup_address) {
+        pr_err("Failed to find lookup_address\n");
+        return -EINVAL;
+    }
+    
+    pr_info("Found lookup_address at %p\n", my_lookup_address);
+
     calling_address = (void *)kallsyms_lookup_name("iterate_dir");
     if (!calling_address)
     {
@@ -45,6 +71,12 @@ static int sys_hooks_init(void)
     //     printk(KERN_ERR "trampoline: Failed to set page for RW\n");
     //     return -EPERM;
     // }
+    
+    if (make_page_writable((unsigned long)calling_address))
+    {
+	printk(KERN_ALERT "trampoline: Unable to make page writable\n");
+	return -1;
+    }
     printk(KERN_INFO "trampoline: set page addr at %016lX as RW\n", hooked_func_page);
 
     relative_hooker_address = (void *)hooker - (calling_address + 5);
