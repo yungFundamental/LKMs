cd63a6c437b2addfb2ffa80a259faa7a23e3af52
Amit Barzilai <amit.barzilai22@gmail.com>
2025-11-16 10:08:41 +0200

    Added hook structure

--- trampoline/main.c
+++ trampoline/main.c
@@ -8,8 +8,20 @@ MODULE_LICENSE("Dual BSD/GPL");
 MODULE_AUTHOR("Amit Barzilai");
 MODULE_DESCRIPTION("Trampoline hook");
 
+
 static int (*orig_iterate_dir)(struct file *, struct dir_context *);
-static 
+
+static int hooked_iterate_dir(struct file *f, struct dir_context *dc)
+{
+    printk(KERN_ALERT "Get hooked douchebag!\n");
+    return orig_iterate_dir(f, dc);
+}
+
+static const hook_t iterate_dir_hook = {
+    .target_name = "iterate_dir",
+    .original_function = (void *)&orig_iterate_dir,
+    .function = hooked_iterate_dir
+};
 
 static int hook_ls_init(void)
 {
